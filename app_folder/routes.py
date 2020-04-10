cbb8d952d16514f81f400ce5e34efe40f8a27486
Jamie Taylor <james.taylor@sjsu.edu>
2020-04-10 16:19:20 -0700

    Created database and made registration form and route

--- app_folder/routes.py
+++ app_folder/routes.py
@@ -1,8 +1,9 @@
 from flask import render_template
 from flask import redirect
 from flask import flash
-from app_folder import app
-from .forms import LoginForm
+from app_folder import app, db
+from .forms import LoginForm, RegisterForm
+from app_folder.models import User, Post
 
 # different URL the app will implement
 @app.route("/")
@@ -28,3 +29,13 @@ def login():
         flash(f'Login requested for user {current_form.username.data}')
         return redirect('/')
     return render_template('login.html', title='Sign In', form=current_form)
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    current_form = RegisterForm()
+    if current_form.validate_on_submit():
+        user = User(current_form.username,current_form.email)
+        db.session.add(user)
+        db.session.commit()
+        return redirect('/')
+    return render_template('register.html', title='Register', form=current_form)
