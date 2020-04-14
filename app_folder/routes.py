53f7b0efaf310e7ba5c4f957d862ce2f607d373f
Vinh Nguyen <vinh.nguyen@sjsu.edu>
2020-04-13 22:36:19 -0700

    Integrated bootstrap and fixed flash messages timing

--- app_folder/routes.py
+++ app_folder/routes.py
@@ -1,6 +1,4 @@
-from flask import render_template
-from flask import redirect
-from flask import flash
+from flask import render_template, redirect, flash, request
 from app_folder import app, db
 from .forms import LoginForm, RegisterForm
 from app_folder.models import User, Post
@@ -25,17 +23,21 @@ def hello():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     current_form = LoginForm()
-    if current_form.validate_on_submit():
+    if current_form.validate(): #check for submission later to avoid flashed message before user logs in
         flash(f'Login requested for user {current_form.username.data}')
         return redirect('/')
+    elif current_form.is_submitted():
+        flash("User has entered an incorrect username or password")
     return render_template('login.html', title='Sign In', form=current_form)
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     current_form = RegisterForm()
-    if current_form.validate_on_submit():
+    if current_form.validate():#check for submission later to avoid flashed message before user registers
         user = User(username = current_form.username.data, email =current_form.email.data,password_hash=current_form.password.data)
         db.session.add(user)
         db.session.commit()
-        flash("Registration was successfull")
+        flash("Registration was successful :)")
+    elif current_form.is_submitted():
+        flash("Registration was not successful :(")
     return render_template('register.html', title='Register', form=current_form)
