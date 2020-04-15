59d8307bc9a8e57cc68ab36f5fdf12d997b67ffd
Jamie Taylor <james.taylor@sjsu.edu>
2020-04-15 15:09:41 -0700

    Fixed issue with redirecting

--- app_folder/routes.py
+++ app_folder/routes.py
@@ -24,21 +24,21 @@ def hello():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
-        return redirect(url_for('index'))
+        return redirect("/")
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
         if user is None or not user.check_password(form.password.data):
             flash('Invalid username or password')
-            return redirect(url_for('login'))
+            return redirect("login")
         login_user(user, remember=form.remember_me.data)
-        return redirect(url_for('index'))
+        return redirect("/")
     return render_template('login.html', title='Sign In', form=form)
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if current_user.is_authenticated:
-        return redirect(url_for('index'))
+        return redirect("/")
     form = RegisterForm()
     if form.validate_on_submit():
         user = User(username=form.username.data, email=form.email.data)
@@ -46,7 +46,7 @@ def register():
         db.session.add(user)
         db.session.commit()
         flash('Congratulations, your account has been created!')
-        return redirect(url_for('login'))
+        return redirect('login')
     return render_template('register.html', title='Register', form=form)
 
 @app.route('/logout')
