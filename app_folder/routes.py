b39a9580430f20f0341c7bb82435f789b0f0e287
Angelo Ibarrola <1105vino@gmail.com>
2020-04-16 16:49:43 -0700

    reworked homepage

--- app_folder/routes.py
+++ app_folder/routes.py
@@ -7,7 +7,6 @@ from flask_login import current_user, login_required, logout_user, login_user
 
 # different URL the app will implement
 @app.route("/")
-@app.route("/home")
 #@login_required
 def hello():
     '''Hello Function.
@@ -55,7 +54,7 @@ def logout():
     
     Will logout the user'''
     logout_user()
-    return redirect("/")
+    return redirect("/home")
 
 @app.route('/viewEvents')
 def viewEvents():
@@ -64,3 +63,7 @@ def viewEvents():
 @app.route('/settings')
 def settings(): 
     return render_template('settings.html', title='settings')
+
+@app.route("/home")
+def home():
+    return render_template('home.html', title='home')
